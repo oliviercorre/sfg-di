6a281720e29107be59c6384cbeb30573383b039b
olcor <olcorre@yahoo.fr>
2022-01-03 22:40:14 +0100

    Spring Configuration Learning

---
+++ src/main/java/com/springframework/pets/DogPetService.java
@@ -0,0 +1,11 @@
+package com.springframework.pets;
+
+/**
+ * Created by jt on 12/28/19.
+ */
+
+public class DogPetService implements PetService {
+    public String getPetType(){
+        return "Dogs are the best!";
+    }
+}
